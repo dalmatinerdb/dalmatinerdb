f1b8582457c294f2acd9845c862b1aa565ffa541
Heinz N. Gies <heinz@licenser.net>
2014-06-15 19:36:37 +0200

    Added glbos.

--- apps/metric_db/src/metric_qry_parser.erl
+++ apps/metric_db/src/metric_qry_parser.erl
@@ -1,7 +1,7 @@
 -module(metric_qry_parser).
 
--export([parse/1, unparse/1, execute/1]).
--ignore_xref([parse/1, unparse/1, execute/1]).
+-export([parse/1, unparse/1, execute/1, glob_match/2]).
+-ignore_xref([parse/1, unparse/1, execute/1, glob_match/2]).
 
 date(L) ->
     i(L).
@@ -112,3 +112,46 @@ i2b(I) ->
 
 f2b(I) ->
     list_to_binary(float_to_list(I)).
+
+prepare_glob(G, Acc) ->
+    case lists:splitwith(fun(X) -> X =/= <<"*">> end, G) of
+        {[], []} ->
+            lists:reverse(Acc);
+        {E, []} ->
+            lists:reverse([E | Acc]);
+        {E, [<<"*">>]} ->
+            lists:reverse([ [], E | Acc]);
+        {H, [_ | T]} ->
+            prepare_glob(T, [H | Acc])
+    end.
+
+
+glob_match(G, Ms) ->
+    GE = re:split(G, "\\."),
+    GL = length(GE),
+    GM = prepare_glob(GE, []),
+    F = fun(M) ->
+                ME = re:split(M, "\\."),
+                ML = length(ME),
+                ML == GL andalso
+                    rmatch(GM, ME)
+        end,
+    lists:filter(F, Ms).
+
+rmatch([[] | R], [_ | RM]) ->
+    rmatch(R, RM);
+rmatch([], []) ->
+    false;
+rmatch([M], M) ->
+    true;
+rmatch([H | T], M) ->
+    case lists:prefix(H, M) of
+        true ->
+            MT = lists:nthtail(length(H)+1, M),
+            rmatch(T, MT);
+        _ ->
+            false
+    end;
+
+rmatch(_, _) ->
+    false.
--- apps/metric_db/test/qry_parser_eqc.erl
+++ apps/metric_db/test/qry_parser_eqc.erl
@@ -11,8 +11,14 @@
 
 -define(P, metric_qry_parser).
 
+non_empty_string() ->
+    ?SUCHTHAT(L, list(choose($a, $z)), L =/= "").
+
+diff_strings() ->
+    ?SUCHTHAT({S1, S2}, {non_empty_string(), non_empty_string()}, S1 =/= S2).
+
 non_empty_binary() ->
-    ?SUCHTHAT(B, ?LET(L, list(choose($a, $z)), list_to_binary(L)), B =/= <<>>).
+    ?LET(L, non_empty_string(), list_to_binary(L)).
 
 
 qry_tree() ->
@@ -30,9 +36,52 @@ qry_tree(Size) ->
               [{sum, int(), qry_tree(Size -1)} || Size > 0] ++
               [{avg, int(), qry_tree(Size -1)} || Size > 0])).
 
+
+glob() ->
+    ?LET({S, G, M}, ?SIZED(Size, glob(Size)),
+         {list_to_binary(string:join(S, ".")),
+          list_to_binary(string:join(G, ".")),
+          M}).
+
+glob(Size) ->
+    ?LAZY(oneof([?LET(S, non_empty_string(), {[S], [S], true}) || Size == 0] ++
+              [add(Size) || Size > 0])).
+
+add(Size) ->
+    frequency(
+      [{1, diff_element(Size)},
+       {10, matching_element(Size)},
+       {10, glob_element(Size)}]).
+
+matching_element(Size) ->
+    ?LAZY(
+       ?LET(
+          {L, G, M}, glob(Size-1),
+          ?LET(S, non_empty_string(),
+               {[S | L], [S | G], M}))).
+
+diff_element(Size) ->
+    ?LAZY(
+       ?LET(
+          {L, G, _M}, glob(Size-1),
+          ?LET({S1, S2}, diff_strings(),
+               {[S1 | L], [S2 | G], false}))).
+
+glob_element(Size) ->
+    ?LAZY(
+       ?LET(
+          {L, G, M}, glob(Size-1),
+          {[non_empty_string() | L], ["*" | G], M})).
+
 prop_qery_parse_unparse() ->
     ?FORALL(T, qry_tree(),
             T == ?P:parse(?P:unparse(T))).
+
+prop_glob_match() ->
+    ?FORALL({S, G, M}, glob(),
+            begin
+                M ==  ([S] == ?P:glob_match(G, [S]))
+            end).
 
 -include("eqc_helper.hrl").
 -endif.
