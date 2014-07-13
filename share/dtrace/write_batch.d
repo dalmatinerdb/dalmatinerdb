6d5dee8e167f79f2aebd86a6058a78790cc050e3
Heinz N. Gies <heinz@licenser.net>
2014-07-13 16:48:54 +0000

    Added dtrace scripts ot benchmark.

---
+++ share/dtrace/write_batch.d
@@ -0,0 +1,34 @@
+#!/usr/sbin/dtrace -s
+/*
+ * arg0 arg1 arg2 arg3 arg4  arg5 arg6   arg7 arg8
+ * PID       ID   Time Size       Module Key  Opperation
+ *
+ * PID -> Erlang process ID. (String)
+ * ID -> ID of the mesurement group, 801 for cowboy handler calls.
+ * Type -> Indicates of this is a entry (1) or return (2).
+ * Found -> 1 if the item was found 2 if not
+ * Module - The module that was called. (string).
+ *
+ * run with: dtrace -s reads.d
+ */
+
+/*
+ * This function gets called every time a erlang developper probe is
+ * fiered, we filter for 801 and 1, so it gets executed when a handler
+ * function is entered.
+ */
+
+erlang$1:::user_trace*
+/ arg2 == 4411 /
+{
+  /*
+   * We cache the relevant strings
+   */
+  @[arg4] = count();
+}
+
+
+tick-1s
+{
+  printa(@);
+}
