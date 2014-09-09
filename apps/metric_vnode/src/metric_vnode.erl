66e6ecb3305aa555c9ea90025f930e63b71749a6
Heinz N. Gies <heinz@licenser.net>
2014-09-09 17:40:21 -0400

    Fixed bug in flush that lead to 1 value writes.

--- apps/metric_vnode/src/metric_vnode.erl
+++ apps/metric_vnode/src/metric_vnode.erl
@@ -336,7 +336,7 @@ do_put(Bucket, Metric, Time, Value, State = #state{tbl = T, ct = CT}) ->
             Bin = k6_bytea:get(Array, 0, Size * 9),
             k6_bytea:set(Array, 0, Value),
             k6_bytea:set(Array, Len * 9, <<0:(9 * 8 * (CT - Len))>>),
-            ets:update_element(T, BM, [{2, Time}, {3, Len}]),
+            ets:update_element(T, BM, [{2, Time}, {3, Len}, {4, Time + Len}]),
             do_write(Bucket, Metric, Start, Bin, State);
         %% In the case the data is already longer then the cache we flush the
         %% cache
@@ -350,7 +350,8 @@ do_put(Bucket, Metric, Time, Value, State = #state{tbl = T, ct = CT}) ->
         [{BM, Start, _Size, _End, Array}] ->
             Idx = Time - Start,
             k6_bytea:set(Array, Idx * 9, Value),
-            ets:update_element(T, BM, [{3, Idx + Len}]),
+            ets:update_element(T, BM,
+                               [{3, Idx + Len}]),
             State;
         %% We don't have a cache yet and our data is smaller then
         %% the current cache limit
