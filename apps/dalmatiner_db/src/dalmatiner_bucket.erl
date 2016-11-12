58f6487fe0d2c4bb6160fd82ab36aa4fc8b749b9
Heinz N. Gies <heinz@licenser.net>
2016-11-12 10:59:37 +0100

    Update for better bucket info

--- apps/dalmatiner_db/src/dalmatiner_bucket.erl
+++ apps/dalmatiner_db/src/dalmatiner_bucket.erl
@@ -10,9 +10,13 @@
 
 -export([list/0, info/1]).
 
+-spec list() -> [binary()].
 list() ->
     Bs = riak_core_metadata:to_list({<<"buckets">>, <<"resolution">>}),
     [B || {B, R} <- Bs, R /= ['$deleted']].
+
+-spec info(binary()) ->
+    dproto_tcp:bucket_info().
 
 info(Bucket) ->
     Resolution = dalmatiner_opt:resolution(Bucket),
