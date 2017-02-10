38391ca5d2628ec58f20c2320202203382a88abc
Tomasz Szarstuk <szarsti@gmail.com>
2017-02-10 12:14:33 +0000

    Pull over some files from master to working branch

--- apps/metric_vnode/src/metric.erl
+++ apps/metric_vnode/src/metric.erl
@@ -2,25 +2,26 @@
 
 -export([
          put/4,
-         mput/3,
+         mput/4,
          get/4,
          get/5,
          delete/1,
          list/1,
          list/2,
-         update_ttl/2
+         update_ttl/2,
+         update_env/0
         ]).
 
--ignore_xref([update_ttl/2, get/4, put/4]).
+-ignore_xref([update_ttl/2, get/4, put/4, update_env/0]).
 
 
-mput(Nodes, Acc, W) ->
+mput(Nodes, Acc, W, N) ->
     folsom_metrics:histogram_timed_update(
       mput, dict, fold,
       [fun(DocIdx, Data, ok) ->
-               do_mput(orddict:fetch(DocIdx, Nodes), Data, W);
+               do_mput(orddict:fetch(DocIdx, Nodes), Data, W, N);
           (DocIdx, Data, R) ->
-               do_mput(orddict:fetch(DocIdx, Nodes), Data, W),
+               do_mput(orddict:fetch(DocIdx, Nodes), Data, W, N),
                R
        end, ok, Acc]).
 
@@ -51,6 +52,9 @@ update_ttl(Bucket, TTL) ->
     dalmatiner_opt:set_lifetime(Bucket, TTL),
     metric_coverage:start({update_ttl, Bucket}).
 
+update_env() ->
+    metric_coverage:start(update_env).
+
 list(Bucket) ->
     folsom_metrics:histogram_timed_update(
       list_metrics, metric_coverage, start, [{metrics, Bucket}]).
@@ -70,20 +74,29 @@ do_put(Bucket, Metric, PPF, Time, Value, N, W) ->
     Preflist = riak_core_apl:get_apl(DocIdx, N, metric),
     ReqID = make_ref(),
     metric_vnode:put(Preflist, ReqID, Bucket, Metric, {Time, Value}),
-    do_wait(W, ReqID).
+    do_wait(W, 0, N, ReqID).
 
-do_mput(Preflist, Data, W) ->
+do_mput(Preflist, Data, W, N) ->
     ReqID = make_ref(),
     metric_vnode:mput(Preflist, ReqID, Data),
-    do_wait(W, ReqID).
+    do_wait(W, 0, N, ReqID).
 
-do_wait(0, _ReqID) ->
+
+%% We got enough replies to be happy
+do_wait(0, _O, _N, _ReqID) ->
     ok;
+%% We did not get enough replies but
+%% every node did reply eithe either with
+%% an overload or a ok.
+do_wait(W, O, N, _ReqID) when W + O == N ->
+    {error, failed};
 
-do_wait(W, ReqID) ->
+do_wait(W, O, N, ReqID) ->
     receive
         {ReqID, ok} ->
-            do_wait(W - 1, ReqID)
+            do_wait(W - 1, O, N, ReqID);
+        {fail, ReqID, _Idx, overload} ->
+            do_wait(W, O + 1, N, ReqID)
     after
         5000 ->
             {error, timeout}
