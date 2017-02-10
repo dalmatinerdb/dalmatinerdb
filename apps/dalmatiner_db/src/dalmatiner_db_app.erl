5be4474a4e7e53f96aea3c0626092f729e55ce1e
Tomasz Szarstuk <szarsti@gmail.com>
2017-02-10 11:38:09 +0000

    Pull over some files from master to working branch

--- apps/dalmatiner_db/src/dalmatiner_db_app.erl
+++ apps/dalmatiner_db/src/dalmatiner_db_app.erl
@@ -3,7 +3,7 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/2, stop/1, wait_for_metadata/0]).
 
 %% ===================================================================
 %% Application callbacks
@@ -14,11 +14,16 @@ start(_StartType, _StartArgs) ->
     folsom_metrics:new_gauge(port_count),
     folsom_metrics:new_gauge(process_count),
     folsom_metrics:new_gauge(tcp_connections),
-    folsom_metrics:new_histogram(put, slide, 60),
-    folsom_metrics:new_histogram({event, put}, slide, 60),
-    folsom_metrics:new_histogram(mput, slide, 60),
     folsom_metrics:new_histogram(get, slide, 60),
+    folsom_metrics:new_histogram(put, slide, 60),
+    folsom_metrics:new_histogram(mput, slide, 60),
+
+    folsom_metrics:new_histogram({event, put}, slide, 60),
     folsom_metrics:new_histogram({event, get}, slide, 60),
+
+    folsom_metrics:new_histogram({mstore, read}, slide, 60),
+    folsom_metrics:new_histogram({mstore, write}, slide, 60),
+
     folsom_metrics:new_histogram(list_buckets, slide, 60),
     folsom_metrics:new_histogram(list_metrics, slide, 60),
     spawn(fun delay_tcp_anouncement/0),
@@ -30,11 +35,13 @@ stop(_State) ->
 delay_tcp_anouncement() ->
     riak_core:wait_for_application(dalmatiner_db),
     Services = riak_core_node_watcher:services(),
+    wait_for_metadata(),
     delay_tcp_anouncement(Services).
 
 delay_tcp_anouncement([S | R]) ->
     riak_core:wait_for_service(S),
     delay_tcp_anouncement(R);
+
 delay_tcp_anouncement([]) ->
     lager:info("[ddb] Enabling TCP listener."),
     Port = case application:get_env(dalmatiner_db, tcp_port) of
@@ -55,3 +62,13 @@ delay_tcp_anouncement([]) ->
                                    [{port, Port},
                                     {max_connections, MaxConn}],
                                    dalmatiner_tcp, []).
+
+%% Wait for the metadata manager to be started
+wait_for_metadata() ->
+    case whereis(riak_core_metadata_manager) of
+        Pid when is_pid(Pid) ->
+            ok;
+        _ ->
+            timer:sleep(500),
+            wait_for_metadata()
+    end.
