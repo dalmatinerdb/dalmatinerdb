c3f51ff9a16a409e996acc2d6aa8a04b2a019813
Heinz N. Gies <heinz@licenser.net>
2017-03-12 20:01:43 +0100

    Add new stream encoder

--- apps/dalmatiner_db/src/dalmatiner_tcp.erl
+++ apps/dalmatiner_db/src/dalmatiner_tcp.erl
@@ -128,6 +128,10 @@ do_send_opt(Socket, Transport, B, M, T, C, Opts) ->
     %% Set the socket to no package control so we can do that ourselfs.
     %% TODO: make this math for configureable length
     %% 8 (resolution + points)
+
+    %% We never send a aggregate for now.
+    Transport:send(Socket, dproto_tcp:encode_get_reply({aggr, undefined});
+
     send_part(Socket, Transport, C0, Points),
     send_parts(Socket, Transport, PPF, B, M, Opts, Splits).
 
@@ -142,13 +146,9 @@ send_parts(Socket, Transport, PPF, B, M, Opts, [{T, C} | Splits]) ->
 
 send_part(Socket, Transport, C, Points) when is_integer(C),
                                               is_binary(Points)->
-    {ok, Compressed} = snappyer:compress(Points),
-    case C - mmath_bin:length(Points) of
-        0 ->
-            Transport:send(Socket, <<1, Compressed/binary>>);
-        Padding ->
-            Transport:send(Socket, <<2, Padding:64/integer, Compressed/binary>>)
-    end.
+    Padding = C - mmath_bin:length(Points),
+    Msg = {data, Points, Padding},
+    Transport:send(Socket, dproto_tcp:encode_get_stream(Msg)).
 
 -spec stream_loop(port(), term(), stream_state(),
                   {dproto_tcp:stream_message(), binary()}) ->
