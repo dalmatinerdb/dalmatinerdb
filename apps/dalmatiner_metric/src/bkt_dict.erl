3881455ddddb0ca36ea0c83876b060142d5dc01b
Heinz N. Gies <heinz@licenser.net>
2018-02-28 16:42:37 +0100

    Use ETS for bkt_dict

--- apps/dalmatiner_metric/src/bkt_dict.erl
+++ apps/dalmatiner_metric/src/bkt_dict.erl
@@ -6,7 +6,17 @@
 
 -export_type([bkt_dict/0]).
 
--record(bkt_dict, {bucket, ppf, dict, n, w, nodes, cbin, size = 0}).
+-record(bkt_dict, {
+          bucket   :: binary(),
+          ppf      :: pos_integer(),
+          dict     :: ets:tid(),
+          counters :: ets:tid(),
+          n        :: pos_integer(),
+          w        :: pos_integer(),
+          nodes    :: list() | undefined,
+          cbin     :: list() | undefined,
+          size = 0 :: non_neg_integer()
+         }).
 
 -type bkt_dict() :: #bkt_dict{}.
 
@@ -14,8 +24,13 @@
                  bkt_dict().
 new(Bkt, N, W) ->
     PPF = dalmatiner_opt:ppf(Bkt),
-    Dict = dict:new(),
-    update_chash(#bkt_dict{bucket = Bkt, ppf = PPF, dict = Dict, n = N, w = W}).
+    Dict = ets:new(bkt_dict, [ordered_set]),
+    Counters = ets:new(bkt_dict_counters, [ordered_set]),
+    update_chash(#bkt_dict{bucket = Bkt,
+                           ppf = PPF,
+                           dict = Dict,
+                           counters = Counters,
+                           n = N, w = W}).
 
 -spec add(binary(), pos_integer(), binary(), bkt_dict()) ->
                  bkt_dict().
@@ -27,10 +42,12 @@ add(Metric, Time, Points, BD = #bkt_dict{ppf = PPF}) ->
 
 -spec flush(bkt_dict()) ->
                  bkt_dict().
-flush(BD = #bkt_dict{dict = Dict, w = W, n = N}) ->
+flush(BD = #bkt_dict{dict = Dict, counters = Counters, w = W, n = N}) ->
     BD1 = #bkt_dict{nodes = Nodes} = update_chash(BD),
     metric:mput(Nodes, Dict, W, N),
-    BD1#bkt_dict{dict = dict:new()}.
+    ets:delete_all_objects(Dict),
+    ets:delete_all_objects(Counters),
+    BD1.
 
 
 -spec update_chash(bkt_dict()) ->
@@ -46,39 +63,33 @@ insert_metric(_Metric, [], <<>>, BD) ->
 
 insert_metric(Metric, [{Time, Count} | Splits], PointsIn,
                BD = #bkt_dict{bucket = Bucket, cbin = CBin, ppf = PPF,
-                              dict = Dict, size = MaxCnt}) ->
+                              dict = Dict, counters = Counters,
+                              size = MaxCnt}) ->
     Size = (Count * ?DATA_SIZE),
     <<Points:Size/binary, Rest/binary>> = PointsIn,
     DocIdx = riak_core_util:chash_key({Bucket, {Metric, Time div PPF}}),
     {Idx, _} = chashbin:itr_value(chashbin:exact_iterator(DocIdx, CBin)),
-    Dict1 = dict:append(Idx, {Bucket, Metric, Time, Points}, Dict),
-
-    CntName = <<Idx:160/integer, "-count">>,
-    BktCnt = case dict:find(CntName, Dict1) of
-                 error ->
-                     1;
-                 {ok, N} ->
-                     N + 1
-             end,
-    Dict2 = dict:store(CntName, BktCnt, Dict1),
-
-    BD1 = BD#bkt_dict{dict = Dict2},
-    BD2 = case BktCnt of
+    L1 = case ets:lookup(Dict, Idx) of
+                [] ->
+                 [{Bucket, Metric, Time, Points}];
+             [{Idx, L0}] ->
+                 [{Bucket, Metric, Time, Points} | L0]
+         end,
+    ets:insert(Dict, {Idx, L1}),
+    BktCnt = ets:update_counter(Counters, Idx, 1, {Idx, 1}),
+    BD1 = case BktCnt of
               BktCnt when BktCnt > MaxCnt ->
-                  BD1#bkt_dict{size = BktCnt};
+                  BD#bkt_dict{size = BktCnt};
               _ ->
-                  BD1
+                  BD
           end,
-    insert_metric(Metric, Splits, Rest, BD2).
+    insert_metric(Metric, Splits, Rest, BD1).
 
 size(#bkt_dict{size = Size}) ->
     Size.
 
 to_list(#bkt_dict{dict = Dict}) ->
-    L = dict:fold(fun (_Idx, Es, Acc) when is_integer(_Idx) ->
-                          [Es | Acc];
-                      (_Idx, _Es, Acc) ->
-                          Acc
+    L = ets:foldl(fun ({_Idx, Es}, Acc) ->
+                          [Es | Acc]
                   end, [], Dict),
     lists:flatten(L).
-
